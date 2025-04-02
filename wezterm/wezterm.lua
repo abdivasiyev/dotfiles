8ac4419c94de62a355cb501c8dc3c8e069f93273
Asliddinbek Azizovich <asliddin.abdivasiyev@gmail.com>
2025-04-02 10:49:26 +0500

    remove bg

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,9 @@
 local wezterm = require("wezterm")
 local config = {
 	enable_tab_bar = false,
-	-- color_scheme = "Catppuccin Mocha",
+	color_scheme = "zenwritten_dark",
 	font = wezterm.font("JetBrains Mono"),
 	font_size = 18.0,
-	window_background_image = os.getenv("HOME") .. "/.config/wezterm/bg/bg.png",
-	window_background_opacity = 0.5,
 	term = "xterm-256color",
 }
 
